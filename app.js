017e290f8e455adfac6593f75af0b64fef3d6ba8
a-tsu <lab_is_truth@yahoo.co.jp>
2021-04-07 19:59:01 +0900

    時間をずらす

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 Push.Permission.request();
-Push.create('こんにちは！', {
-　　body: '更新をお知らせします！', 
-　　icon: 'icon.png',
-　　timeout: 8000, // 通知が消えるタイミング 
-　　vibrate: [100, 100, 100], // モバイル端末でのバイブレーション秒数 
-　　onClick: function() {
-　　　　// 通知がクリックされた場合の設定 
-　　　　console.log(this);
-　　}
-});
+
+// 10秒後に通知
+window.setTimeout(function(){
+    Push.create('こんにちは！', {
+    　　body: '通知をお知らせします！', 
+    　　icon: 'icon.png',
+    　　timeout: 8000, // 通知が消えるタイミング 
+    　　vibrate: [100, 100, 100], // モバイル端末でのバイブレーション秒数 
+    　　onClick: function() {
+    　　　　// 通知がクリックされた場合の設定 
+    　　　　console.log(this);
+    　　}
+    });
+}, 5000);
